19ed0c229de2ffd90847034e98cabf46a877bb89
Sasha <sashatsapko91@gmail.com>
2025-06-11 21:25:50 +0300

    FirstSetup

---
+++ setup.js
@@ -0,0 +1,6 @@
+import {client} from "./src/utils/db.js";
+import {User} from "./src/models/user.js";
+
+client.sync({
+  force: true,
+});
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+const PORT = process.env.PORT || 3005;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server was started! on port ${PORT}`);
+});
